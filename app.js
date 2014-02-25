50f7623b7a026b3525f80f71a7ddbb2790f81dbc
Fredrik Bystam <fredrik.bystam@gmail.com>
2014-02-25 16:35:12 +0100

    börjar lika något nu...

--- app.js
+++ app.js
@@ -7,12 +7,29 @@ app.set('view engine', 'jade');
 app.use(express.logger('dev'));
 
 app.use(express.static(__dirname + '/public'))
-
+app.use(express.urlencoded());
 
 app.get('/', function(req, res) {
-	res.render('index', {
-		title : 'Småsyskonöverlämning 2014'
+	res.render('mission', {
+		title : 'Småsyskonöverlämning 2014',
+		id : 1
     });
+});
+
+var validateMissionPassphase = function (req, callback) {
+	var result = {};
+	if (req.body.passphrase === 'lolfi golfi') {
+		result.url = "google.com";
+	} else {
+		result.url = "felsomfan.se";
+	}
+	callback(result);
+}
+
+app.post('/', function (req, res) {
+	validateMissionPassphase(req, function (result) {
+		console.log(result.url);
+	});
 });
 
 var port = process.env.PORT || 5000;
